70ee236ecba23c38ab02c8538ddcee19ce6f0ff0
Tatiana Viana <tatisabinoviana@gmail.com>
2021-06-15 11:18:53 -0300

    TEmplate String e expressão regular implantada

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,19 +11,14 @@ class NegociacaoController {
     adiciona(event) {
         
         event.preventDefault();
-        
-        // '2016-11-12'
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item  - indice % 2)
-        );
+
         let negociacao = new Negociacao(
-            data, 
+            DateHelper.textoParaData(this._inputData.value), 
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 }
